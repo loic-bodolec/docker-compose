b25cb3293d0b3766d9ed0d988e141a88bd369c27
loicbdev <loicbodolec@msn.com>
2022-01-18 11:51:25 +0100

    update: Dockerfile (ENV) front

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>hello les loulous!</h1>
+        <h1>hello les wilders!</h1>
       </header>
     </div>
   );
